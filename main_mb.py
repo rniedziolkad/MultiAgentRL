fac80548d5dae9a1c3e3be1eeee3cf5b8d30f062
Rafal <rniedziolkad@gmail.com>
2026-01-17 12:32:35 +0100

    using pursuit environment

--- main_mb.py
+++ main_mb.py
@@ -1,26 +1,30 @@
-from pettingzoo.mpe import simple_spread_v3
+from pettingzoo.sisl import pursuit_v4
 from model_based.agent import MBAgent
 import numpy as np
 from matplotlib import pyplot as plt
 import torch
 import time
+from functools import reduce
 
-N_AGENTS = 9
+N_AGENTS = 8
 
 MAX_EPISODES = 1_000_001
-MAX_STEPS = 25
+MAX_STEPS = 500
 BATCH_SIZE = 32
 
-env = simple_spread_v3.parallel_env(N=N_AGENTS, max_cycles=MAX_STEPS, render_mode="none")
+env = pursuit_v4.parallel_env(n_pursuers=N_AGENTS, max_cycles=MAX_STEPS, render_mode="none")
 env.reset(seed=42)
-agents = [MBAgent(name, env.observation_space(name).shape[0], env.action_space(name).n, eps_end=0.0001, eps_decay=10000)
+agents = [MBAgent(name, reduce(lambda x, y: x*y, env.observation_space(name).shape), env.action_space(name).n,
+                  eps_end=0.0001, eps_decay=10000)
           for name in env.agents]
 
 print(agents)
-print("cuda" if torch.cuda.is_available() else "cpu")
+print("cpu")
 rewards_history = []
 for episode in range(MAX_EPISODES):
     obs, _ = env.reset()
+    for a in obs.keys():
+        obs[a] = obs[a].flatten()
     total_reward = 0
     t0 = time.perf_counter()
 
@@ -31,6 +35,8 @@ for episode in range(MAX_EPISODES):
             actions[agent.name] = action
 
         next_obs, rewards, terminations, truncations, _ = env.step(actions)
+        for a in next_obs.keys():
+            next_obs[a] = next_obs[a].flatten()
         for agent in agents:
             agent.replay.add((
                 torch.as_tensor(obs[agent.name], device=agent.device, dtype=torch.float32),
@@ -53,7 +59,7 @@ for episode in range(MAX_EPISODES):
     t1 = time.perf_counter()
     print("time:", t1 - t0)
     rewards_history.append(total_reward)
-    if (episode + 1) % 500 == 0:
+    if (episode + 1) % 25 == 0:
         # plotting rolling avg rewards of agent 0
         avg_rewards = np.sum(rewards_history[-100:]) / len(rewards_history[-100:])
         plt.clf()
@@ -65,7 +71,7 @@ for episode in range(MAX_EPISODES):
         plt.savefig(f"mb_target{N_AGENTS}agents.png")
         # saving data for later
         torch.save(rewards_history, f'mb_rewards_history_target{N_AGENTS}agents.pth')
-    if episode % 10_000 == 0:
+    if episode % 500 == 0:
         for agent in agents:
             agent.save_model(f"model_based/saved_models{N_AGENTS}/ep"+str(episode)+"/")
 
